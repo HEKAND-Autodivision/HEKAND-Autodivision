3ec335b7fa506c4af0391206eecb6429774597f1
HEKAND-Autodivision <andhiandrianto94@gmail.com>
2026-08-14 16:19:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ if (!process.env.VERCEL) {
 }
 
 const SUPABASE_URL = process.env.SUPABASE_URL || '';
-const SUPABASE_KEY = process.env.SUPABASE_ANON_KEY || '';
+const SUPABASE_KEY =
+  process.env.SUPABASE_SERVICE_ROLE_KEY ||
+  process.env.SUPABASE_ANON_KEY ||
+  '';
 
 const supabase =
   SUPABASE_URL && SUPABASE_KEY
